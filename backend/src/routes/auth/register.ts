c15aa6b77356cd5417be454253529771b972151c
Francois Gonzalez <francois.gnz@gmail.com>
2026-04-24 20:02:08 +0200

    database user working

--- backend/src/routes/auth/register.ts
+++ backend/src/routes/auth/register.ts
@@ -1,20 +1,47 @@
 import { Request, Response } from 'express';
 import * as z from "zod"
+import { prisma } from '../../server';
 
 
 const RegisterSchema = z.object({
-    username: z.string().min(3),
-    password: z.string().min(8)
+    username: z
+        .string("Field is required")
+        .min(3, {
+            error: (iss) => {
+                iss.minimum;
+                iss.inclusive;
+                return `Username must have ${iss.minimum} characters or more`
+            }
+        }),
+    password: z
+        .string("Field is required")
+        .min(8, {
+            error: (iss) => {
+                iss.minimum;
+                iss.inclusive;
+                return `Password must have ${iss.minimum} characters or more`
+            }
+        })
 })
 
 export async function registerHandler(req: Request, res: Response) {
     try
     {
         const result = RegisterSchema.safeParse({ username: req.body.username, password: req.body.password })
+        const zodErrors = result.error?.issues.map((i) => i.message)
+        
         if (!result.success)
-            throw new Error("Zod validation failed")
+            return res.status(400).json({success: false, message: zodErrors})
             
             
+        await prisma.user.create({
+            data: {
+                username: "Hello",
+                email: "test@gmail.com",
+                password: "world"
+            }
+        })
+        
         res.status(201).json({ success: true, message: "Succesfully registered !" });
     }
     catch (error)
